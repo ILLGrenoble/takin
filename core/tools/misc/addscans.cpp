01790fcbb31f0213b10e580236d2620dc49d0ebf
Tobias WEBER <tweber@ill.fr>
2025-10-06 16:28:09 +0200

    misc: generalised addscan tool

--- core/tools/misc/addscans.cpp
+++ core/tools/misc/addscans.cpp
@@ -25,7 +25,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  * ----------------------------------------------------------------------------
  */
-// gcc -I../.. -I. -DNO_IOSTR -o addscans ../../tlibs/file/loadinstr.cpp ../../tlibs/log/log.cpp addscans.cpp -std=c++11 -lstdc++ -lm -lboost_system -lboost_filesystem
+// g++ -I../.. -I. -DNO_IOSTR -o addscans ../../tlibs/file/loadinstr.cpp ../../tlibs/log/log.cpp addscans.cpp -std=c++17 -lboost_system -lboost_filesystem
 // e.g. ./addscans /home/tweber/Auswertungen/MnSi-Mira-15/data3/11009_00016851.dat /home/tweber/Auswertungen/MnSi-Mira-15/data3/11009_00016867.dat merged.dat
 
 #include <iostream>
@@ -45,36 +45,37 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
-	tl::FileFrm<> dat0;
-	tl::log_info("Loading file ", argv[1]);
-	if(!dat0.Load(argv[1]))
+	tl::log_info("Loading file ", argv[1], "...");
+	tl::FileInstrBase<t_real> *dat0 = tl::FileInstrBase<t_real>::LoadInstr(argv[1]);
+	if(!dat0)
 	{
 		tl::log_err("Cannot load data file ", argv[1], ".");
 		return -1;
 	}
 
-	std::string strCnt = dat0.GetCountVar();
-	std::string strMon = "mon2"; //dat0.GetMonVar();
+	std::string strCnt = dat0->GetCountVar();
+	std::string strMon = dat0->GetMonVar();
+	//std::string strMon = "M2";
 	tl::log_info("Count var: ", strCnt, ", monitor var: ", strMon);
 
-	tl::FileInstrBase<>::t_vecVals& vecCnt0 = dat0.GetCol(strCnt);
-	tl::FileInstrBase<>::t_vecVals& vecMon0 = dat0.GetCol(strMon);
+	tl::FileInstrBase<>::t_vecVals& vecCnt0 = dat0->GetCol(strCnt);
+	tl::FileInstrBase<>::t_vecVals& vecMon0 = dat0->GetCol(strMon);
 
 
-	for(int iArg=2; iArg<argc-1; ++iArg)
+	for(int iArg = 2; iArg < argc - 1; ++iArg)
 	{
 		const char* pcFile = argv[iArg];
 
-		tl::FileFrm<> dat;
-		tl::log_info("Loading file ", pcFile);
-	        if(!dat.Load(pcFile))
+		tl::log_info("Loading file ", pcFile, "...");
+		tl::FileInstrBase<t_real> *dat = tl::FileInstrBase<t_real>::LoadInstr(pcFile);
+		if(!dat)
 		{
 			tl::log_err("Cannot load data file ", pcFile, ".");
 			return -1;
 		}
 
-		const tl::FileInstrBase<>::t_vecVals& vecCnt = dat.GetCol(strCnt);
-		const tl::FileInstrBase<>::t_vecVals& vecMon = dat.GetCol(strMon);
+		const tl::FileInstrBase<>::t_vecVals& vecCnt = dat->GetCol(strCnt);
+		const tl::FileInstrBase<>::t_vecVals& vecMon = dat->GetCol(strMon);
 
 		if(vecCnt.size() != vecCnt0.size() || vecMon.size() != vecMon0.size())
 		{
@@ -82,11 +83,13 @@ int main(int argc, char **argv)
 			return -1;
 		}
 
-		for(unsigned int i=0; i<vecCnt0.size(); ++i)
+		for(unsigned int i = 0; i < vecCnt0.size(); ++i)
 		{
 			vecCnt0[i] += vecCnt[i];
 			vecMon0[i] += vecMon[i];
 		}
+
+		delete dat;
 	}
 
 
@@ -99,17 +102,18 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
-	ofstr.precision(16);
-	const tl::FileInstrBase<>::t_vecColNames& vecColNames = dat0.GetColNames();
-	for(unsigned int i=0; i<vecCnt0.size(); ++i)
+	ofstr.precision(8);
+	const tl::FileInstrBase<>::t_vecColNames& vecColNames = dat0->GetColNames();
+	for(unsigned int i = 0; i < vecCnt0.size(); ++i)
 	{
 		for(const std::string& strColName : vecColNames)
 		{
-			tl::FileInstrBase<>::t_vecVals& vecCol = dat0.GetCol(strColName);
+			tl::FileInstrBase<>::t_vecVals& vecCol = dat0->GetCol(strColName);
 			ofstr << std::setw(20) << vecCol[i];
 		}
 		ofstr << "\n";
 	}
 
+	delete dat0;
 	return 0;
 }
