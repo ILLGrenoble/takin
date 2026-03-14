238e03d52a4aac12bbe0faad44944edbddb2f048
Tobias Weber (Institut Laue-Langevin) <tweber@ill.fr>
2026-03-14 19:30:23 +0100

    Update vio_ext.cpp

--- core/tools/res/algos/vio_ext.cpp
+++ core/tools/res/algos/vio_ext.cpp
@@ -1,5 +1,5 @@
 /**
- * re-implementation of V. Mecoli's extension of Violini's TOF reso algorithm [mec25, mec25b]
+ * re-implementation of V. Mecoli's extension of Violini's TOF reso algorithm [mec25]
  * @author Tobias Weber <tobias.weber@tum.de>
  * @modif_by Victor Mecoli <mecoli@ill.fr> - nov-2025
  * @date oct-2025
